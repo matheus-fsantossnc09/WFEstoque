2edd63af8894b67a65db6dec2c8fb5708a3bde81
matheus-fsantossnc09 <matheus.fsantos72@senacsp.edu.br>
2025-04-02 14:46:07 -0300

    teste

--- WFCadastroProduto/FormMenu.Designer.cs
+++ WFCadastroProduto/FormMenu.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormMenu";
+            SuspendLayout();
+            // 
+            // FormMenu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MdiChildrenMinimizedAnchorBottom = false;
+            Name = "FormMenu";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "FormMenu";
+            ResumeLayout(false);
         }
 
         #endregion
